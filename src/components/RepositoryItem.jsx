a4921cfaacf9420b6f3756682d81a3ecdaead23b
Thiago <thiagosantosprofissional@gmail.com>
2021-03-15 00:22:57 -0300

    feat: Transformando App e RepositoryItem de JSX para TSX.

--- src/components/RepositoryItem.jsx
+++
@@ -1,9 +0,0 @@
-export function RepositoryItem(props) {
-  return (
-    <li>
-      <strong>{props.repository.name ?? 'Default'}</strong>
-      <p>{props.repository.description ?? 'Default'}</p>
-      <a href={props.repository.html_url ?? 'Default'}>Acessar repositório</a>
-    </li>
-  );
-}
